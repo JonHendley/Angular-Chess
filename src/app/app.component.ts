7bcd141b3db7667cf5df32d2252d7e529b55a26c
Jonathan Hendley <hendleyjon@gmail.com>
2022-02-10 13:17:59 -0500

    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 
 export enum Pieces {
+  Possible,
   Empty,
   BlackPawn,
   BlackBishop,
@@ -25,7 +26,7 @@ export enum Pieces {
 
 export class AppComponent {
   title = 'Angular-Chess';
-  icons: String[] = ["assets/chessPieces/blank.png", "assets/chessPieces/b_pawn_png_512px.png", "assets/chessPieces/b_bishop_png_512px.png", "assets/chessPieces/b_knight_png_512px.png",
+  icons: String[] = ["assets/chessPieces/possible.png", "assets/chessPieces/blank.png", "assets/chessPieces/b_pawn_png_512px.png", "assets/chessPieces/b_bishop_png_512px.png", "assets/chessPieces/b_knight_png_512px.png",
     "assets/chessPieces/b_rook_png_512px.png", "assets/chessPieces/b_queen_png_512px.png", "assets/chessPieces/b_king_png_512px.png",
     "assets/chessPieces/w_pawn_png_512px.png", "assets/chessPieces/w_bishop_png_512px.png", "assets/chessPieces/w_knight_png_512px.png",
     "assets/chessPieces/w_rook_png_512px.png", "assets/chessPieces/w_queen_png_512px.png", "assets/chessPieces/w_king_png_512px.png"];
@@ -39,7 +40,7 @@ export class AppComponent {
     [Pieces.WhitePawn, Pieces.WhitePawn, Pieces.WhitePawn, Pieces.WhitePawn, Pieces.WhitePawn, Pieces.WhitePawn, Pieces.WhitePawn, Pieces.WhitePawn],
     [Pieces.WhiteRook, Pieces.WhiteKnight, Pieces.WhiteBishop, Pieces.WhiteQueen, Pieces.WhiteKing, Pieces.WhiteBishop, Pieces.WhiteKnight, Pieces.WhiteRook]
   ];
-  playerColor = "";
+  playerColor = "White";
   playersTurn = true;
   pieceSelectedX: number = -1;
   pieceSelectedY: number = -1;
@@ -62,11 +63,96 @@ export class AppComponent {
   }
 
   selectPiece(x: number, y: number) {
-    console.log(x, " ", y);
+    if (this.playersTurn) {
+      if (this.board[y][x] == Pieces.Possible) {
+        this.movePiece(x, y);
+      } else {
+        this.resetPossibilities();
+        if (this.playerColor == "Black" && (this.board[y][x] > 0 && this.board[y][x] < 7)) {
+          this.pieceSelectedX = x;
+          this.pieceSelectedY = y;
+        } else if (this.playerColor == "White" && this.board[y][x] > 6) {
+          this.pieceSelectedX = x;
+          this.pieceSelectedY = y;
+        }
+        this.findPossibilities(x, y);
+      }
+    }
   }
 
-  isPieceSelected(x: number, y: number): boolean {
-    if (this.pieceSelectedX === x && this.pieceSelectedY === y) {
+  movePiece(x: number, y: number) {
+    this.board[y][x] = this.board[this.pieceSelectedY][this.pieceSelectedX];
+    this.board[this.pieceSelectedY][this.pieceSelectedX] = Pieces.Empty;
+    this.pieceSelectedX = x;
+    this.pieceSelectedY = y;
+    for (let i = 0; i < 8; i++) {
+      for (let j = 0; j < 8; j++) {
+        if (this.board[i][j] == Pieces.Possible) {
+          this.board[i][j] = Pieces.Empty;
+        }
+      }
+    }
+    //this.playersTurn = !this.playersTurn;
+  }
+
+  resetPossibilities() {
+    for (let i = 0; i < 8; i++) {
+      for (let j = 0; j < 8; j++) {
+        if (this.board[i][j] == Pieces.Possible) {
+          this.board[i][j] = Pieces.Empty;
+        }
+      }
+    }
+  }
+
+  findPossibilities(x: number, y: number) {
+    switch (this.board[y][x]) {
+      case Pieces.BlackPawn:
+      case Pieces.WhitePawn:
+        if (y == 6) {
+          this.board[y - 1][x] = Pieces.Possible;
+          this.board[y - 2][x] = Pieces.Possible;
+        }
+        break;
+
+      case Pieces.BlackRook:
+      case Pieces.WhiteRook:
+        for (let index = y + 1; index < 8; index++) {
+          if (this.board[index][x] == Pieces.Empty) {
+            this.board[index][x] = Pieces.Possible;
+          } else {
+            break;
+          }
+        }
+        for (let index = y - 1; index >= 0; index--) {
+          if (this.board[index][x] == Pieces.Empty) {
+            this.board[index][x] = Pieces.Possible;
+          } else {
+            break;
+          }
+        }
+        for (let index = x + 1; index < 8; index++) {
+          if (this.board[y][index] == Pieces.Empty) {
+            this.board[y][index] = Pieces.Possible;
+          } else {
+            break;
+          }
+        }
+        for (let index = x - 1; index >= 0; index--) {
+          if (this.board[y][index] == Pieces.Empty) {
+            this.board[y][index] = Pieces.Possible;
+          } else {
+            break;
+          }
+        }
+        break;
+
+      default:
+        break;
+    }
+  }
+  isThisPieceSelected(x: number, y: number): boolean {
+    if (this.playersTurn && this.pieceSelectedX === x && this.pieceSelectedY === y) {
       return true;
     } else {
       return false;
